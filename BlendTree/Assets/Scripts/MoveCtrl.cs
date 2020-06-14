fe5ac23dd0e152a8b0df53c37f14a1e44feffe6a
unknown <1294838346@qq.com>
2020-06-14 17:23:11 +0800

    混合树

---
+++ BlendTree/Assets/Scripts/MoveCtrl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCtrl : MonoBehaviour
+{
+	public Animator animator;
+	public float horizontal;
+	public float vertical;
+	// Use this for initialization
+	void Start()
+	{
+		this.animator = this.GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		horizontal = Input.GetAxis("Horizontal");
+		vertical = Input.GetAxis("Vertical");
+		animator.SetFloat("dir", horizontal, 0.1f, Time.deltaTime);
+
+		animator.SetFloat("speed", vertical, 0.1f, Time.deltaTime);
+	}
+}
